e28cfc0cf0ded46e840d348b8fdd901da3242c3a
AugustinRyan <80559513+augustinryan11@users.noreply.github.com>
2022-06-29 14:44:13 +0800

    Latest Codes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -29,7 +29,7 @@ $app->withEloquent();
 
 $app->configure('services');
 $app->configure('auth');
-
+$app->configure('database');
 /*
 |--------------------------------------------------------------------------
 | Register Container Bindings
